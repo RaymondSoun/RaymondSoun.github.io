1b5d614cc3bcc928f7bc94f3eff3fe6238704a96
RaymondSoun <raymond.soundar@gmail.com>
2017-03-10 18:55:45 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,28 +1,10 @@
-function webservice(url,id)
-{
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = this.responseText;
         var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
-        document.getElementById("id").innerHTML = jsonPretty;
+        document.getElementById("demo").innerHTML = jsonPretty;
     }
 };
-xmlhttp.open("GET", url, true);
+xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main", true);
 xmlhttp.send();
-}
-
-function homePageLoading() {
-    url = 'http://bitcoin.mubiz.com/info';
-    elementID = 'info';
-    fonctionRequeteApi(url, elementID);
-    url = 'http://bitcoin.mubiz.com/blockchaininfo';
-    elementID = 'info';
-    fonctionRequeteApi(url, elementID);
-    url = 'http://bitcoin.mubiz.com/peerinfo';
-    elementID = 'info';
-    fonctionRequeteApi(url, elementID);
-    url = 'http://bitcoin.mubiz.com/mininginfo';
-    elementID = 'info';
-    fonctionRequeteApi(url, elementID);
-}
