eb032025ce862ce84cf25077f15d42a4a31310dc
RaymondSoun <raymond.soundar@gmail.com>
2017-03-10 18:34:31 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,28 @@
+function webservice(url,id)
+{
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = this.responseText;
         var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
-        document.getElementById("demo").innerHTML = jsonPretty;
+        document.getElementById("id").innerHTML = jsonPretty;
     }
 };
-xmlhttp.open("GET", "http://bitcoin.mubiz.com/address/bitcoin_address/", true);
+xmlhttp.open("GET", url, true);
 xmlhttp.send();
+}
+
+function homePageLoading() {
+    url = 'http://bitcoin.mubiz.com/info';
+    elementID = 'info';
+    fonctionRequeteApi(url, elementID);
+    url = 'http://bitcoin.mubiz.com/blockchaininfo';
+    elementID = 'info';
+    fonctionRequeteApi(url, elementID);
+    url = 'http://bitcoin.mubiz.com/peerinfo';
+    elementID = 'info';
+    fonctionRequeteApi(url, elementID);
+    url = 'http://bitcoin.mubiz.com/mininginfo';
+    elementID = 'info';
+    fonctionRequeteApi(url, elementID);
+}
